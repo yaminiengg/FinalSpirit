a0ded7f11940489ea0f52d2826a07e21219cb3f9
yamini <124370154+yaminiengg@users.noreply.github.com>
2026-03-22 14:10:18 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,20 +1,21 @@
 const mysql = require("mysql2");
 
-/*const db = mysql.createConnection({
+const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.MYSQLPORT
 });
-*/
 
-const db = mysql.createConnection({
+/*const db = mysql.createConnection({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
   port: process.env.MYSQLPORT
 });
+*/
 
 db.connect((err) => {
   if (err) {
