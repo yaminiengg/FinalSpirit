d87ce6f6dafd2dc3f9b2fc6232f3fd007b9c0660
yamini <124370154+yaminiengg@users.noreply.github.com>
2026-03-22 16:57:08 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql2");
 
 console.log("🔄 Trying DB connection...");
-/*
+
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -9,9 +9,9 @@ const db = mysql.createConnection({
   database: process.env.DB_NAME,
   port: process.env.MYSQLPORT
 });
-*/
+/*
 const db = mysql.createConnection(process.env.MYSQL_PUBLIC_URL);
-
+*/
 db.connect((err) => {
   if (err) {
     console.log("Database connection failed:", err);
